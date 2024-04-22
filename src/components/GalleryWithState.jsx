9990e748ff1e46ff248cbfd2161203b97533bd30
mehdibehvar <mehdibehvar00@gmail.com>
2024-04-22 07:57:16 +0330

    with-state

--- src/components/GalleryWithState.jsx
+++ src/components/GalleryWithState.jsx
@@ -1,35 +1,34 @@
-import { sculptureList } from '../data.js';
-import { useState } from 'react';
+import { sculptureList } from "../data.js";
+import { useState } from "react";
 export default function GalleryWithState() {
- const [index, setIndex] = useState(0)
-
+  const [index, setIndex] = useState(0);
+  const [showmore, setShowmore] = useState(false);
   function handleClick() {
-    setIndex(index + 1)
+    setIndex(index + 1);
+  }
+  function handleShowmore() {
+    setShowmore(!showmore);
   }
 
   let sculpture = sculptureList[index];
   console.log(index);
   return (
     <>
-<div className="glallery-card">
-<button onClick={handleClick}>
-        Next
-      </button>
-      <h2>
-        <i>{sculpture.name} </i> 
-        by {sculpture.artist}
-      </h2>
-      <h3>  
-        ({index + 1} of {sculptureList.length})
-      </h3>
-      <img 
-        src={sculpture.url} 
-        alt={sculpture.alt}
-      />
-      <p>
-        {sculpture.description}
-      </p>
-</div>
+      <div className="glallery-card">
+        <button onClick={handleClick}>Next</button>
+        <h2>
+          <i>{sculpture.name} </i>
+          by {sculpture.artist}
+        </h2>
+        <h3>
+          ({index + 1} of {sculptureList.length})
+        </h3>
+        <button onClick={handleShowmore}>
+          {showmore ? "hide" : "show"}details
+        </button>
+        <img src={sculpture.url} alt={sculpture.alt} />
+        {showmore && <p>{sculpture.description}</p>}
+      </div>
     </>
   );
 }
